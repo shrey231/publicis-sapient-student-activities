e624dcfeb66ba804d94d8ffed1e52035d24182a9
Shreyas Vaderiyattil <shrvader@publicisgroupe.net>
2022-06-23 15:58:33 -0400

    base

---
+++ Day12/daythreeExample/src/main/java/com/example/DaythreeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaythreeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaythreeExampleApplication.class, args);
+	}
+
+}
